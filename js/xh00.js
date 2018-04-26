dd1fab92bd91641f0958f33bbdb457adf71afd42
lol <30472772+xh00@users.noreply.github.com>
2018-04-26 11:15:53 +0100

    Add files via upload

---
+++ js/xh00.js
@@ -0,0 +1,18 @@
+function fadeOutPageGithub() {
+  $("body").fadeOut(500);
+  setTimeout(function(){
+    location.assign("https://github.com/xh00/");
+  }, 500);
+}
+function fadeOutPageUpdateTxt() {
+  $("body").fadeOut(500);
+  setTimeout(function(){
+    location.assign("update.txt");
+  }, 500);
+}
+var txtLoad = 'https://xh00.tk/update.txt';
+  $(document).ready(function() {
+    $.get(txtLoad, function (data) {
+      document.getElementById("txtFile").innerHTML = data;
+    });
+  });
