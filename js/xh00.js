44e56d60c54c2fbfd20db12648c5b02eed069e55
lol <30472772+xh00@users.noreply.github.com>
2018-04-26 11:33:28 +0100

    Update xh00.js
    - Indentation fixes

--- js/xh00.js
+++ js/xh00.js
@@ -1,18 +1,18 @@
 function fadeOutPageGithub() {
-  $("body").fadeOut(500);
-  setTimeout(function(){
-    location.assign("https://github.com/xh00/");
-  }, 500);
+    $("body").fadeOut(500);
+    setTimeout(function() {
+        location.assign("https://github.com/xh00/");
+    }, 500);
 }
 function fadeOutPageUpdateTxt() {
-  $("body").fadeOut(500);
-  setTimeout(function(){
-    location.assign("update.txt");
-  }, 500);
+    $("body").fadeOut(500);
+    setTimeout(function() {
+        location.assign("update.txt");
+    }, 500);
 }
 var txtLoad = 'https://xh00.tk/update.txt';
-  $(document).ready(function() {
+$(document).ready(function() {
     $.get(txtLoad, function (data) {
-      document.getElementById("txtFile").innerHTML = data;
+        document.getElementById("txtFile").innerHTML = data;
     });
-  });
+});
